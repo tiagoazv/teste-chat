cbee4968f188aecc1b3234cb80d6df938ccd2759
Tiago Azevedo <tgoncalvesazevedo@gmail.com>
2025-07-27 15:44:52 -0300

    style: aplica Tailwind no projeto

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,12 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './hooks/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
